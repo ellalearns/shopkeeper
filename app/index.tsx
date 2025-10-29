7fd052de24453989a8b4dd72fec14db6ee24a7a1
ellalearns <ellalearnscopy@gmail.com>
2025-10-29 12:07:18 +0100

    set up project

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { splashStyles } from "@/styles";
+import { Image, ImageBackground } from "react-native";
+
+export default function Index() {
+  return (
+    <ImageBackground style={splashStyles.main}>
+      <Image source={} />
+    </ImageBackground>
+  );
+}
---
+++ styles/index.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const splashStyles = StyleSheet.create({
+    main: {
+        backgroundColor: "#e7e4d1",
+        flex: 1
+    }
+})
