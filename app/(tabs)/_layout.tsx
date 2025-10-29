49ad457a0d6f7e3f5a0649f86b8919fbf448ac41
ellalearns <ellalearnscopy@gmail.com>
2025-10-29 14:31:48 +0100

    style bottom nav

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,38 +1,69 @@
+import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
 
+const TabIcon = ({ focused, activeIcon, icon }: any) => {
+    return (
+        <View style={[{
+            width: "220%",
+            alignItems: "center",
+            justifyContent: "center",
+            alignSelf: "center",
+            height: "120%",
+            borderRadius: 40
+        }, focused && {backgroundColor: "#dfc87c"}]}>
+            <Image source={focused ? activeIcon : icon} style={focused ? null : null} />
+        </View>
+    )
+}
 
 export default function _Layout() {
     return (
         <Tabs
             screenOptions={{
-                headerShown: false
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: "transparent",
+                    marginHorizontal: 25,
+                    position: "absolute",
+                    bottom: 20,
+                    elevation: 0,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderTopWidth: 0
+                }
             }}>
-                
+
             <Tabs.Screen
                 name="home"
                 options={{
-                    title: "Home"
+                    title: "Home",
+                    tabBarIcon: ({ focused }) => <TabIcon focused={focused} activeIcon={images.activeHome} icon={images.home} />
                 }}
             />
 
             <Tabs.Screen
                 name="add"
                 options={{
-                    title: "Add"
+                    title: "Add",
+                    tabBarIcon: ({ focused }) => <TabIcon focused={focused} activeIcon={images.activePlus} icon={images.plus} />
                 }}
             />
 
             <Tabs.Screen
                 name="categories"
                 options={{
-                    title: "Categories"
+                    title: "Categories",
+                    tabBarIcon: ({ focused }) => <TabIcon focused={focused} activeIcon={images.activeCategory} icon={images.category} />
                 }}
             />
 
             <Tabs.Screen
                 name="user"
                 options={{
-                    title: "User"
+                    title: "User",
+                    tabBarIcon: ({ focused }) => <TabIcon focused={focused} activeIcon={images.activeUser} icon={images.user} />
                 }}
             />
         </Tabs>
